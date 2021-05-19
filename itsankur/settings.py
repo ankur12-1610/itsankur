9f3a1dd73a1817b29efd5eee4f1ed356fd9152c6
ankur12-1610 <76884959+ankur12-1610@users.noreply.github.com>
2021-05-19 11:37:32 +0530

    Changes

--- itsankur/settings.py
+++ itsankur/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wc&ndgu8%6hz!_w#6motwfn_(z_csb$po78(b$m2sb)$=odu6r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['itsankurs.herokuapp.com']
 
 
 # Application definition
